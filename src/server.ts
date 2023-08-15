86222389b0cfb276b4dd6a18221e6be96bf2ffc9
JoeMakuta <makutajosue@gmail.com>
2023-08-15 15:23:33 +0200

    Refa

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the CarDocBox Api !");
+  res.send("Welcome to the CarDocBox Api by JoeMakuta!");
 });
 
 app.listen(port, () => {
