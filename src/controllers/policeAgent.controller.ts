1aee0ae76555f28624e7269f5309812fa14bd031
JoeMakuta <makutajosue@gmail.com>
2023-08-18 17:50:47 +0200

    Feat : validate the agent inputs

--- src/controllers/policeAgent.controller.ts
+++ src/controllers/policeAgent.controller.ts
@@ -2,29 +2,35 @@ import express from "express";
 import { Request, Response, NextFunction } from "express";
 import * as httpError from "http-errors";
 import PoliceAgentModel from "../models/policeAgent.model";
+import validate_police from "../validation/policeAgent.valid";
 
 export default class PoliceAgent {
-  static async home(req: Request, res: Response, next: NextFunction) {
-    res.json(<IServerResponse>{
-      status: 200,
-      data: null,
-      message: "Police Agent : Home",
-      error: null,
-    });
-  }
+  // static async home(req: Request, res: Response, next: NextFunction) {
+  //   res.json(<IServerResponse>{
+  //     status: 200,
+  //     data: null,
+  //     message: "Police Agent : Home",
+  //     error: null,
+  //   });
+  // }
 
   static async add(req: Request, res: Response, next: NextFunction) {
     try {
-      const response = await PoliceAgentModel.create({
-        ...req.body,
-      });
-      if (response) {
-        res.status(200).json(<IServerResponse>{
-          status: 200,
-          data: { ...req.body },
-          message: "Police Agent created successfully",
-          error: null,
+      const valid = validate_police(req.body);
+      if (valid.error) {
+        throw new httpError.Forbidden(valid.error.details[0].message);
+      } else {
+        const response = await PoliceAgentModel.create({
+          ...req.body,
         });
+        if (response) {
+          res.status(200).json(<IServerResponse>{
+            status: 200,
+            data: { ...req.body },
+            message: "Police Agent created successfully",
+            error: null,
+          });
+        }
       }
     } catch (error) {
       res.status(500).json(<IServerResponse>{
@@ -43,8 +49,8 @@ export default class PoliceAgent {
       if (response) {
         res.status(200).json(<IServerResponse>{
           status: 200,
-          data: response,
           message: "All Police Agents",
+          data: response,
           error: null,
         });
       }
--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -3,8 +3,7 @@ import PoliceAgent from "../controllers/policeAgent.controller";
 
 const adminRoute: express.Router = Router();
 
-adminRoute.get("/", PoliceAgent.home);
+adminRoute.get("/", PoliceAgent.getAll);
 adminRoute.post("/signup", PoliceAgent.add);
-adminRoute.post("/all", PoliceAgent.getAll);
 
 export default adminRoute;
