1a6b3a14773f544a7b76e797d9a0154d42051a7c
JoeMakuta <makutajosue@gmail.com>
2023-09-18 09:22:12 +0200

    Feat : deploy

---
+++ src/@types/payLoad.type.ts
@@ -0,0 +1,4 @@
+export interface IPayLoad {
+  id: string;
+  email: string;
+}
--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -4,20 +4,22 @@ import { IUserRequest } from "../@types/user.type";
 import PoliceAgentModel from "../models/policeAgent.model";
 import * as httpError from "http-errors";
 import { Model } from "sequelize";
+import { IPayLoad } from "../@types/payLoad.type";
 
 const verifyToken = async (
-  req: IUserRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   const { TOKEN_SECRET } = process.env;
   try {
     const token: string = req.headers.authorization?.split(" ")[1] as string;
-    const payLoad = jwt.verify(token, TOKEN_SECRET as string);
-
-    const user: Model<any, any> | null = await PoliceAgentModel.findByPk(
-      payLoad.id
+    const payLoad: string | jwt.JwtPayload = jwt.verify(
+      token,
+      TOKEN_SECRET as string
     );
+
+    const user = await PoliceAgentModel.findByPk(payLoad!.id);
     if (user) {
       req.auth = user;
       next();
